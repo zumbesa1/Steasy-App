44ea0b3dc93cb55fd762400b67f8a3cc13eb98d8
Salahudin Zumberi <zumbesa1@students.zhaw.ch>
2020-05-27 19:45:49 +0200

    deleted unnecessary funcitons

--- steasy_app/lib/main.dart
+++ steasy_app/lib/main.dart
@@ -173,17 +173,6 @@ class MySteasyState extends State<MyBluetoothApp> with SingleTickerProviderState
   }
 
 
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
